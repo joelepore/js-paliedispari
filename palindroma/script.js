50ba1d2429f83c0face74547f88f55da820f6019
joelepore <giuseppejoelepore@gmail.com>
2024-11-19 15:10:08 +0100

    feat: complete palindroma exercise

---
+++ palindroma/script.js
@@ -0,0 +1,33 @@
+/*
+  Chiedere all’utente di inserire una parola
+  Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+let message;
+
+const inputWord = prompt('Inserisci una parola.');
+
+if (isPalindroma(inputWord)) {
+  message = `${inputWord} e' una parola palindroma.`;
+} else {
+  message = `${inputWord} NON e' una parola palindroma.`;
+}
+
+console.log(message);
+
+function isPalindroma(word) {
+  if (word === reverse(word)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function reverse(word) {
+  let outputWord = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    outputWord += word[i];
+  }
+
+  return outputWord;
+}
